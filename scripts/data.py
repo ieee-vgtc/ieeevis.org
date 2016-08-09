17a8efee15b22de1ac7df9428283d088ca03eb89
Carlos Scheidegger <cscheid@cs.arizona.edu>
2016-08-09 11:02:35 -0600

    starting to move scripts from other repo to here

---
+++ scripts/data.py
@@ -0,0 +1,79 @@
+"""
+A thin RDBMS-like layer on top of google spreadsheets.
+
+Carlos Scheidegger and Sam Gratzl, 2016
+
+We recommend you use this under a virtual environment. Create
+a virtualenv and then install the required libraries with
+
+$ pip install -r requirements.txt
+
+If you need to run this locally, please contact Sam or Carlos for
+the private key to access the spreadsheet from the script.
+
+"""
+
+import json
+import gspread
+from oauth2client.client import SignedJwtAssertionCredentials
+
+##############################################################################
+# Data Loading
+
+def load_credentials():
+    json_key = json.load(open('files/service-account-key.json'))
+    scope = ['https://spreadsheets.google.com/feeds']
+
+    credentials = SignedJwtAssertionCredentials(json_key['client_email'], json_key['private_key'].encode(), scope)
+    return credentials
+
+def context(credentials):
+    return gspread.authorize(credentials)
+
+def load_sheet_by_name(gc, name):
+    result = [v for v in gc.worksheets() if v.title == name]
+    if len(result) == 0: raise Exception("Could not find '%s' sheet" % name)
+    if len(result) > 1: raise Exception("Too many '%s' sheets" % name)
+    return result[0]
+
+##############################################################################
+# Poor man's RDBMS
+
+def inner_join(lst1, lst2, column):
+    result = []
+    index_lst2 = dict((v[column], v) for v in lst2)
+    for el in lst1:
+        if el[column] in index_lst2:
+            row = el.copy()
+            row.update(index_lst2[el[column]])
+            result.append(row)
+    return result
+
+def group_by(lst, selector):
+    result = {}
+    for item in lst:
+        key = selector(item)
+        if key not in result:
+            result[key] = []
+        result[key].append(item)
+    return [{'Key': key, 'Value': value} for (key, value) in result.iteritems()]
+
+def group_by_pairs(lst, selector):
+    result = []
+    for d in group_by(lst, selector):
+        result.append((d['Key'], d['Value']))
+    return result
+
+def recolumn(lst, old_column, new_column):
+    result = []
+    for item in lst:
+        item = item.copy()
+        v = item[old_column]
+        del item[old_column]
+        item[new_column] = v
+        result.append(item)
+    return result
+
+def column(name):
+    def f(item): return item[name]
+    return f
---
+++ scripts/write_committee_html.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python
+
+"""
+Write committee tables to HTML from the Google Spreadsheet.
+
+Carlos Scheidegger and Sam Gratzl, 2016
+
+We recommend you use this under a virtual environment. Create
+a virtualenv and then install the required libraries with
+
+$ pip install -r requirements.txt
+
+If you need to run this locally, please contact Sam or Carlos for
+the private key to access the spreadsheet from the script.
+
+"""
+
+from data import *
+
+import min_html as h
+import sys
+
+##############################################################################
+
+def add_committee(el, committee):
+    def add_people(people_list):
+        added = set()
+        def last_name(s):
+            if s['Name'] == '':
+                return ''
+            return s['Name'].split()[-1]
+        sys.stderr.write(str(people_list) + '\n')
+        for p in sorted(people_list, key=last_name):
+            name = p['Name']
+            if name == '' or name in added:
+                continue
+            added.add(name)
+            institution = p['Institution']
+            if p.get('SubConference', '').strip() <> '':
+                institution = '%s (%s)' % (institution, p['SubConference'])
+            el.add_child(h.tr(h.td(name), h.td(institution)))
+        
+    name = committee['RoleName']
+    el.add_child(h.tr(h.th(name, colspan="2"), h.th(h.nbsp), **{"class": "committee_section"}))
+    sys.stderr.write(str(committee) + '\n')
+    people = dict(group_by_pairs(committee['Value'], column("SubConference")))
+
+    for subconference in ['VIS', 'VAST', 'InfoVis', 'SciVis']:
+        add_people(people.get(subconference, []))
+        if people.has_key(subconference): del people[subconference]
+    for remaining_subconference, people_list in people.iteritems():
+        add_people(people_list)
+
+gc = context(load_credentials()).open("VIS2016_roster_clean")
+people = load_sheet_by_name(gc, "People").get_all_records()
+roles =  load_sheet_by_name(gc, "Roles").get_all_records()
+
+el = h.table(**{"class": "committee_list", "border": "0"})
+body = h.tbody()
+el.add_child(body)
+
+records = inner_join(recolumn(group_by(people, column("Role")), 'Key', 'Role'), roles, "Role")
+
+result = dict((r['Role'], r) for r in records)
+
+order = ["General", "Vice", "Program", "Paper", "Poster", "Panel", "Tutorial", "Workshop", "ArtShow", "Challenge", "Contest",
+         "VDS", "LDAV", "VizSec", "BioVis", "BELIV", "Vis in Practice", "Doctoral Colloquium", "Video/FF", "Meetup", "Community",
+         "Student Vol", "Publicity", "VisKids", "Liaison", "Supporters", "Finance", "Publication", "Web"]
+
+for committee in order:
+    add_committee(body, result[committee])
+
+sys.stdout.write(el.render().encode('utf-8') + '\n')
+
