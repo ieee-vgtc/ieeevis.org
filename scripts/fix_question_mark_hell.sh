3f40d8189a1ea4b9b0cbaf7ab93e1ce6bee2d0e6
Carlos Scheidegger <cscheid@cs.arizona.edu>
2016-09-17 20:38:28 -0700

    css hell

--- scripts/fix_question_mark_hell.sh
+++ scripts/fix_question_mark_hell.sh
@@ -6,4 +6,10 @@ for f in `find . -type f -exec grep -q '\.js?M' {} \; -and -print`; do
     cp /tmp/foo $f
 done
 
+for f in `find . -type f -exec grep -q '\.css?M' {} \; -and -print`; do
+    echo Processing $f.
+    cat $f | sed 's/\.css?M/.css/g' > /tmp/foo
+    cp /tmp/foo $f
+done
+
 echo OK, done.
