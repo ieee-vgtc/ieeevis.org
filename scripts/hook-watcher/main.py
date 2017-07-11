ea173922195b4221e859a5114a77eb0d61f3542f
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-07-11 12:30:56 -0700

    same for production now...

--- scripts/hook-watcher/main.py
+++ scripts/hook-watcher/main.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def handle():
     content = json.loads(request.form["payload"])
-    print content
     if content["ref"] == "refs/heads/master":
         print "something was pushed to master!"
         subprocess.call("./build-staging")
+    elif content["ref"] == "refs/heads/production":
+        print "something was pushed to production!"
+        subprocess.call("./build-production")
     return "OK."
 
 if __name__ == '__main__':
