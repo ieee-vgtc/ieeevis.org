e1a4077f14ed311b34782b3fb5d92ff581b6cbf0
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-07-11 12:24:12 -0700

    testing actual hook

--- scripts/hook-watcher/main.py
+++ scripts/hook-watcher/main.py
@@ -1,13 +1,16 @@
 from flask import Flask, request
 import json
+import subprocess
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def handle():
     content = json.loads(request.form["payload"])
-    # if content["hook"]["head"] == "refs/heads/master":
-    #     print "something was pushed to master!"
-    return "OK"
+    print content
+    if content["ref"] == "refs/heads/master":
+        print "something was pushed to master!"
+        subprocess.call("./build-staging")
+    return "OK."
 
 if __name__ == '__main__':
     app.run(port=1234)
