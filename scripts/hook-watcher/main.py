ebe11d3af817c62b951c87c8f4554a73fc5f7d9f
Carlos Scheidegger <cscheid@cs.arizona.edu>
2017-07-11 12:10:38 -0700

    more

--- scripts/hook-watcher/main.py
+++ scripts/hook-watcher/main.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
+import json
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
 def handle():
-    content = request.get_json(silent=True)
-    print content
+    content = json.reads(request.form["payload"])
+    if content["head"] == "refs/heads/master":
+        print "something was pushed to master!"
     return "OK"
 
 if __name__ == '__main__':
