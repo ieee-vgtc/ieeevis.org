5a16ef76bc8b6e753f58f2dab7f6c23eddf397a0
lane <ltharrison@wpi.edu>
2017-08-18 14:25:36 -0400

    support bitmap

--- scripts/convert.sh
+++ scripts/convert.sh
@@ -5,6 +5,7 @@ cd attachments/supporters/2017/dl
 
 for file in *.eps; do convert  -colorspace rgb $file "`basename $file .eps`.png"; done
 for file in *.jpg; do convert $file "`basename $file .jpg`.png"; done
+for file in *.BMP; do convert $file "`basename $file .BMP`.png"; done
 for file in *.pdf; do convert  -colorspace rgb $file "`basename $file .pdf`.png"; done
 
 for file in *.png; do mv $file converted/; done
