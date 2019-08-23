f0a8217d41e98cd14572b1fa5cb73d9c54db9859
Alper Sarikaya <yelper@gmail.com>
2019-08-23 00:21:14 -0700

    updated paper sessions; resolves #768

--- scripts/write_program_md.py
+++ scripts/write_program_md.py
@@ -19,6 +19,7 @@ from data import *
 
 import min_html as h
 import sys
+import codecs
 
 gc = get_spreadsheet("web-VIS2019-Program")
 records = load_sheet_by_name(gc, "Program").get_all_records()
@@ -41,6 +42,18 @@ venue_name = { "cga": "CG&A",
                "infovis": "InfoVis",
                "scivis": "SciVis",
                "vast": "VAST" }
+venue_abbr = { "cga": "",
+               "vast": "[V] ",
+               "infovis": "[I] ",
+               "scivis": "[S] " }
+
+def paper_conf(paper):
+    id = paper["ID"].lower()
+    conf = id[:id.find('-')]
+    if conf in venue_abbr.keys():
+        return venue_abbr[conf]
+    else:
+        return ""
 
 def paper_type(paper):
     t = paper["Type"]
@@ -69,19 +82,19 @@ def render_session(session, out):
     name = session["Key"]
     # venue = venue_name[guess_venue(session)]
     venue = session_dict[session["Key"]]["Venue"]
-    out.write("**%s**  \n" % session_dict[name]["Day"].upper())
+    out.write("<a name=\"%s\">**%s**</a>  \n" % (session_dict[name]['SessionID'], session_dict[name]["Day"].upper()))
     out.write("**%s**  \n" % session_dict[name]["Time"])
     out.write("**Room: %s**  \n" % session_dict[name]["Room"])
-    if venue is not None:
-        out.write("*%s: %s*  \n" % (venue, name))
-    else:
-        out.write("*%s  \n" % name)
+    # if venue is not None:
+    #     out.write("*%s: %s*  \n" % (venue, name))
+    # else:
+    out.write("*%s*  \n" % name)
     out.write("*Session Chair: %s*  \n" % (session_dict[name]["Chair"]))
     out.write("\n")
     for paper in session["Value"]:
         award = awards.get(paper["ID"], None)
-        out.write(("**%s%s**%s  \n" % (paper["Title"], paper_type(paper), award_string(award))).encode("utf-8"))
-        out.write(("Authors: %s\n" % paper["Author list"]).encode("utf-8"))
+        out.write(("**%s%s%s**%s  \n" % (paper_conf(paper), paper["Title"], paper_type(paper), award_string(award))))
+        out.write(("Authors: %s\n" % paper["Author list"]))
         out.write("\n")
     out.write("<hr/>\n\n")
 
@@ -99,7 +112,8 @@ def session_key(session):
     time = order[metadata["Time"][:metadata["Time"].find(':')]]
     return (day, time)
 
-out = sys.stdout
+# out = sys.stdout
+out = codecs.open('output/program.txt', 'w', 'utf8')
 session_papers = group_by(records, lambda k: k['Session'])
 for session in sorted(session_papers, key = lambda session: session_dict[session['Key']]['Order']):
     render_session(session, out)
