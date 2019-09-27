b64712e63f73e2b471a62af93b0841bd650560f0
Jan Rubio <rubiojan@gmail.com>
2019-09-27 08:18:50 -0400

    NO-REF

--- src/index.js
+++ src/index.js
@@ -7,17 +7,60 @@ document.addEventListener('DOMContentLoaded', () => {
         el: '#navigation',
         data: function() {
           return {
+            bodyElement: null,
+            isCollapsed: false,
+            isCollapsedPrevious: false,
             isExpanded: false,
             isAriaHidden: true,
             isAriaExpanded: false,
             menu: {}
           };
         },
+        mounted: function () {
+          this.isCollapsed = window.innerWidth < 768;
+          this.isCollapsedPrevious = window.innerWidth < 768;
+          this.bodyElement = document.getElementsByTagName('BODY')[0];
+          window.addEventListener('resize', this.debounce(this.handleResize, 25));
+        },
+        destroyed: function () {
+          window.removeEventListener('resize', this.debounce(this.handleResize, 25));
+        },
         methods: {
+          debounce: function (fn, delay) {
+            let timeoutID = null;
+            return function () {
+              clearTimeout(timeoutID);
+              const args = arguments;
+              const that = this;
+              timeoutID = setTimeout(function () {
+                fn.apply(that, args);
+              }, delay);
+            };
+          },
+
+          handleResize: function () {
+            this.isCollapsed = window.innerWidth < 768;
+            const hasMenuItemsOpen = this.hasMenuItemsOpen();
+
+            // Check if the browser resize caused a change in layout for the menu
+            if (this.isCollapsedPrevious !== this.isCollapsed) {
+              this.isExpanded = hasMenuItemsOpen;
+
+              if (this.isCollapsed && hasMenuItemsOpen) {
+                this.bodyElement.classList.add('overflow-hidden');
+              } else {
+                this.bodyElement.classList.remove('overflow-hidden');
+              }
+
+              this.isCollapsedPrevious = window.innerWidth < 768;
+              this.$forceUpdate();
+            }
+          },
+
           toggleMenu: function () {
             this.isExpanded = !this.isExpanded;
+            this.togglePreventBodyScroll();
             this.resetMenuItems();
-            console.warn(this.isExpanded);
             this.$forceUpdate();
           },
 
@@ -41,6 +84,22 @@ document.addEventListener('DOMContentLoaded', () => {
                 this.menu[key] = false;
               }
             }
+          },
+
+          hasMenuItemsOpen: function () {
+            for (let key in this.menu) {
+              if (this.menu.hasOwnProperty(key)) {
+                if (this.menu[key]) {
+                  return true;
+                }
+              }
+            }
+            return false;
+          },
+
+          togglePreventBodyScroll: function () {
+            const body = document.getElementsByTagName('BODY')[0];
+            body.classList.toggle('overflow-hidden');
           }
         }
     });
