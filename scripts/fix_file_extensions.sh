0d2bd15cb8f10f1f67c5823f1c5394d9378dfb1e
lane <ltharrison@wpi.edu>
2019-10-16 20:00:00 -0400

    baseurl test3

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `find _site/year/{2016,2017,2018,2019} _site/content _site/governance -name "*.html"`; do
+for i in `find _site/year/{2016,2017,2018,2019} _site/info _site/governance -name "*.html"`; do
     mv $i `echo $i | sed s/.html$//`;
 done
 
