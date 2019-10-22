2ca269012c2d5a75bc84040750790231a5f3a49e
lane <ltharrison@wpi.edu>
2019-10-22 14:55:06 -0700

    prep for 2020

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-for i in `find _site/year/{2016,2017,2018,2019} _site/info _site/governance -name "*.html"`; do
+for i in `find _site/year/{2016,2017,2018,2019,2020} _site/governance -name "*.html"`; do
     mv $i `echo $i | sed s/.html$//`;
 done
 
 # Move straggling pages
 
 mv _site/instructions.html _site/instructions
-mv _site/logos.html _site/logos
-mv _site/keynote.html _site/keynote
-mv _site/capstone.html _site/capstone
-mv _site/videos.html _site/videos
 mv _site/welcome.html _site/welcome
 mv _site/styleguide.html _site/styleguide
 
