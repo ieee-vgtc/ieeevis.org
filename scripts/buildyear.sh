317d3d8037ccd5db95b818ef6c36a331436ab814
lane <ltharrison@wpi.edu>
2019-10-22 14:53:52 -0700

    prep for 2020

--- scripts/buildyear.sh
+++ scripts/buildyear.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
 mkdir _site/year
-mkdir _site/year/2019
+mkdir _site/year/2020
 
-mv _site/assets _site/year/2019
-mv _site/capstone _site/year/2019
-mv _site/videos _site/year/2019
-mv _site/content/info/2019-schedule.svg _site/year/2019/info
-mv _site/content _site/year/2019
-mv _site/dist _site/year/2019
-mv _site/info _site/year/2019
-mv _site/instructions _site/year/2019
-mv _site/keynote _site/year/2019
-mv _site/styleguide _site/year/2019
-mv _site/tailwind.config.js _site/year/2019
-mv _site/webpack.common.js _site/year/2019
-mv _site/webpack.dev.js _site/year/2019
-mv _site/webpack.prod.js _site/year/2019
-mv _site/welcome _site/year/2019
+mv _site/assets _site/year/2020
+mv _site/content _site/year/2020
+mv _site/dist _site/year/2020
+mv _site/info _site/year/2020
+mv _site/tailwind.config.js _site/year/2020
+mv _site/webpack.common.js _site/year/2020
+mv _site/webpack.dev.js _site/year/2020
+mv _site/webpack.prod.js _site/year/2020
+mv _site/welcome _site/year/2020
 
 
