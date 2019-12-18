3ca34c8a4573212e25d6b18ed1a7b28c33bf3205
Alper Sarikaya <yelper@gmail.com>
2019-12-18 00:35:10 -0800

    removing old pages

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -4,9 +4,3 @@ for i in `find _site/year/{2016,2017,2018,2019,2020} _site/governance -name "*.h
     mv $i `echo $i | sed s/.html$//`;
 done
 
-# Move straggling pages
-
-[ -f _site/instructions.html ] && mv _site/instructions.html _site/instructions
-[ -f _site/welcome.html ] && mv _site/welcome.html _site/welcome
-[ -f _site/styleguide.html ] && mv _site/styleguide.html _site/styleguide
-
