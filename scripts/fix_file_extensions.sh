1f3d03d5e42a214201d39050566e89524006cf47
Alper Sarikaya <yelper@gmail.com>
2019-12-18 22:24:05 -0800

    missed mv operand

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -6,7 +6,7 @@ done
 
 # overload governance with 2020 style
 rm -rf _site/governance
-mv _site/year/2020/governance 
+mv _site/year/2020/governance -t _site/
 
 # move 2020-specific 404/error pages to root
 cp _site/year/2020/error* _site/year/2020/404 -t _site/
