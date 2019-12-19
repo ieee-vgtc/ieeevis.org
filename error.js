a6312b33e65b6ded3187ee71b104eeac8bb08411
Alper Sarikaya <yelper@gmail.com>
2019-12-18 22:15:47 -0800

    some QOL fixes (404 page, footer, governance)

--- error.js
+++ error.js
@@ -1,5 +1,5 @@
 function send_to_create_gh_flow() {
-  var base = "https://github.com/ieee-vgtc/ieeevis.org/new/master";
+  var base = "https://github.com/ieee-vgtc/ieeevis.org/new/vis2020";
   var p = base + window.location.pathname;
   var i = p.lastIndexOf("/");
   var filename = p.substr(i + 1) + ".md";
@@ -8,7 +8,7 @@ function send_to_create_gh_flow() {
   var yaml_front_matter;
   yaml_front_matter = ["---",
     "title: " + p.substr(i + 1),
-    "layout: main-2019",
+    "layout: page",
     "permalink: " + window.location.pathname,
     "---\n\n"];
   yaml_front_matter = yaml_front_matter.join("\n");
--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -4,3 +4,9 @@ for i in `find _site/year/{2016,2017,2018,2019,2020} _site/governance -name "*.h
     mv $i `echo $i | sed s/.html$//`;
 done
 
+# overload governance with 2020 style
+rm -rf _site/governance
+mv _site/year/2020/governance 
+
+# move 2020-specific 404/error pages to root
+cp _site/year/2020/error* _site/year/2020/404 -t _site/
