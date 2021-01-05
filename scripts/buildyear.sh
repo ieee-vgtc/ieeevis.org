b0183b73fde1297cbcb198bdd83e59f0b67a3e9c
Steve Petruzza <steve.petruzza@gmail.com>
2021-01-05 09:33:44 -0700

    trying fix governance deploy

--- scripts/buildyear.sh
+++ scripts/buildyear.sh
@@ -7,6 +7,7 @@ mv _site/assets _site/year/2021
 mv _site/content _site/year/2021
 mv _site/dist _site/year/2021
 mv _site/info _site/year/2021
+mv _site/governance _site/year/2021
 mv _site/tailwind.config.js _site/year/2021
 mv _site/webpack.common.js _site/year/2021
 mv _site/webpack.dev.js _site/year/2021
