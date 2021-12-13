d8a88b93bcc8d0462f8b1e4691152003f49dcc85
Dylan Cashman <depcashman@gmail.com>
2021-12-13 12:02:53 -0500

    Adjust fix_file_extensions.sh to use 2022

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for i in `find _site/year/{2016,2017,2018,2019,2020,2021} _site/governance -name "*.html"`; do
+for i in `find _site/year/{2016,2017,2018,2019,2020,2021,2022} _site/governance -name "*.html"`; do
     mv $i `echo $i | sed s/.html$//`;
 done
 
-# overload governance with 2021 style
+# overload governance with 2022 style
 rm -rf _site/governance
-mv _site/year/2021/governance -t _site/
+mv _site/year/2022/governance -t _site/
 
-# move 2021-specific 404/error pages to root
-cp _site/year/2021/error* _site/year/2021/404 -t _site/
+# move 2022-specific 404/error pages to root
+cp _site/year/2022/error* _site/year/2022/404 -t _site/
 cp _site/error _site/error.html
