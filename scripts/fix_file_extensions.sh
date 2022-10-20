a957b36d7a5ca26d49520dd69afafebadf0cd8e9
Dylan Cashman <depcashman@gmail.com>
2022-10-20 09:08:32 -0500

    more switches from 2022 to 2023

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-for i in `find _site/year/{2016,2017,2018,2019,2020,2021,2022} _site/governance -name "*.html"`; do
+for i in `find _site/year/{2016,2017,2018,2019,2020,2021,2022, 2023} _site/governance -name "*.html"`; do
     mv $i `echo $i | sed s/.html$//`;
 done
 
 # overload governance with 2022 style
 rm -rf _site/governance
-mv _site/year/2022/governance -t _site/
+mv _site/year/2023/governance -t _site/
 
 # move 2022-specific 404/error pages to root
-cp _site/year/2022/error* _site/year/2022/404 -t _site/
+cp _site/year/2023/error* _site/year/2023/404 -t _site/
 cp _site/error _site/error.html
