df8d5db45ee19b407117aacaf84761bded875a0c
Dylan Cashman <depcashman@gmail.com>
2023-02-20 21:35:29 -0500

    Remove another package from 2019

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const glob = require('glob');
 const TerserJSPlugin = require('terser-webpack-plugin');
-const PurgecssPlugin = require('purgecss-webpack-plugin');
 const CompressionPlugin = require('compression-webpack-plugin');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
@@ -23,16 +22,6 @@ module.exports = merge(common, {
     },
 
     plugins: [
-        new CompressionPlugin(),
-
-        new PurgecssPlugin({
-            paths: glob.sync(path.join(__dirname, '{_includes,_layouts,content}') + '/**/*', { nodir: true }),
-            extractors: [
-                {
-                    extractor: TailwindExtractor,
-                    extensions: ['html']
-                }
-            ]
-        })
+        new CompressionPlugin()
     ]
 });
