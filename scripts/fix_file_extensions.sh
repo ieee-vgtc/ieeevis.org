8e220a862bb6b7c762663eeeb8b76cfbb8429d76
Dylan Cashman <depcashman@gmail.com>
2024-07-31 17:29:53 -0400

    More fiddling with file extensions

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `find _site/year/{2024} _site/governance -name "*.html"`; do
+for i in `find _site/year/2024 _site/governance -name "*.html"`; do
     mv $i `echo $i | sed s/.html$//`;
 done
 
@@ -11,8 +11,8 @@ mv _site/year/2024/governance _site/
 # overload static assets for vis virtual
 # rm -rf _site/static
 # mv _site/year/2024/static _site/
-cp -r _site/year/2024/static/* _site/static/*
+# cp -r _site/year/2024/static/* _site/static/*
 
 # move 2022-specific 404/error pages to root
-cp _site/year/2024/error* _site/year/2024/404 -t _site/
-cp _site/error _site/error.html
+# cp _site/year/2024/error* _site/year/2024/404 -t _site/
+# cp _site/error _site/error.html
