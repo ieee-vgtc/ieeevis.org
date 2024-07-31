120816c7b065b73b3583f521d0cb598719ceace7
Dylan Cashman <depcashman@gmail.com>
2024-07-31 17:22:04 -0400

    Another fix for file extensions script, trying to fix urls in deploy

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in `find _site/year/{2016,2017,2018,2019,2020,2021,2022,2023,2024} _site/governance -name "*.html"`; do
+for i in `find _site/year/{2024} _site/governance -name "*.html"`; do
     mv $i `echo $i | sed s/.html$//`;
 done
 
@@ -9,8 +9,9 @@ rm -rf _site/governance
 mv _site/year/2024/governance _site/
 
 # overload static assets for vis virtual
-rm -rf _site/static
-mv _site/year/2024/static _site/
+# rm -rf _site/static
+# mv _site/year/2024/static _site/
+cp -r _site/year/2024/static/* _site/static/*
 
 # move 2022-specific 404/error pages to root
 cp _site/year/2024/error* _site/year/2024/404 -t _site/
