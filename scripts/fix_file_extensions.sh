e66dd8904f01a82df16b7ba4c65a93e65abd2917
Dylan Cashman <depcashman@gmail.com>
2024-10-03 00:07:53 -0400

    Trying to fix posters upload page

--- scripts/fix_file_extensions.sh
+++ scripts/fix_file_extensions.sh
@@ -10,7 +10,9 @@ rm -rf _site/year/2024/static
 for i in `find _site/year/{2016,2017,2018,2019,2020,2021,2022,2023,2024} _site/governance -name "*.html"`; do
     if [[ $i != *"/papers.html" ]]; then
         if [[ $i != *"program/paper_"* ]]; then
-            mv $i `echo $i | sed s/.html$//`;
+            if [[ $i != *"/posters.html" ]]; then
+                mv $i `echo $i | sed s/.html$//`;
+            fi
         fi
     fi
 done
