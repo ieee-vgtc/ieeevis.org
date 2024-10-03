e6f55a4a6f6da34625e162546383273e937c96f5
Shayan Monadjemi <monadjemis@ornl.gov>
2024-10-03 10:51:23 -0400

    auth on other pages

--- src/index.js
+++ src/index.js
@@ -4,28 +4,28 @@ import Vue from 'vue'
 import { createAuth0Client } from "@auth0/auth0-spa-js";
 
 var getUrlParameter = function getUrlParameter(sParam) {
-    var sPageURL = window.location.search.substring(1),
-        sURLVariables = sPageURL.split('&'),
-        sParameterName,
-        i;
+  var sPageURL = window.location.search.substring(1),
+    sURLVariables = sPageURL.split('&'),
+    sParameterName,
+    i;
 
-    console.log("sPageURL is ", sPageURL);
-    console.log("sURLVariables is ", sURLVariables);
-    for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+  console.log("sPageURL is ", sPageURL);
+  console.log("sURLVariables is ", sURLVariables);
+  for (i = 0; i < sURLVariables.length; i++) {
+    sParameterName = sURLVariables[i].split('=');
 
-        if (sParameterName[0] === sParam) {
-            return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
-        }
+    if (sParameterName[0] === sParam) {
+      return sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
     }
-    return false;
+  }
+  return false;
 };
 
 document.addEventListener('DOMContentLoaded', async () => {
 
 
   // console.log("URL parameter is ", getUrlParameter('loginMsg'), " and is it true? ", getUrlParameter('loginMsg') == true);
-    
+
   if (getUrlParameter('loginMsg')) {
     $("#loginToastMessage").show();
   }
@@ -241,41 +241,28 @@ document.addEventListener('DOMContentLoaded', async () => {
         body.classList.toggle('overflow-hidden');
       }
     },
-    // mounted: () => {
-    //   console.log(window.location.origin)
-    //   try {
-    //     auth0.isAuthenticated().then(function (authenticated) {
-    //       console.log(authenticated, 'authenticated')
-    //     })
-    //   } catch (err) {
-    //     console.log("Log in failed", err);
-    //   }
+    mounted: () => {
+      console.log('is mounted')
+      const auth0_domain = 'ieeevis.us.auth0.com'
+      const auth0_client_id = 'G8onz2A6h59RmuYFUbSLpGmxsGHOyPOv'
+      //console.log("origin is " + window.location.origin);
+      createAuth0Client({
+        domain: auth0_domain,
+        clientId: auth0_client_id,
+        cacheLocation: "localstorage"
+      }).then(async (auth0Client) => {
+        const isAuthenticated = await auth0Client.isAuthenticated();
+        const query = window.location.search;
+        const userProfile = await auth0Client.getUser();
 
+        console.log(isAuthenticated, 'authenticated')
+        console.log(userProfile)
 
-
-    //   // const is_auth = await auth0.isAuthenticated();
-    //   // console.log(is_auth, "--- is_auth");
-    //   // if (is_auth) {
-    //   // console.log(is_auth)
-    //   //   // document.body.style.display = null;
-
-    //   //   // unused atm, hook up later; this won't get executed since we change location above
-    //   //   const user = await auth0.getUser();
-    //   //   console.log(user)
-    //   //   // $(".loginBtn").hide();
-    //   //   // $(".logoutBtn").show();
-    //   //   // $(".secret").show();
-    //   //   // $(".user_name").text(user.name);
-    //   //   // $(".login-message").text("You are logged in as ");
-    //   //   // $("#download-proceedings-div").html("You can download the proceedings from <a href='https://www.dropbox.com/scl/fo/7smc635unf8ohm9gu8sds/h?rlkey=kacc5v1v9tcdekehkb7lg03fj&dl=0' target='_blank'>this Dropbox link</a> using the password mel23.  Or, use this <a href='https://drive.google.com/drive/folders/1zWW8PPiQenVeUu7jaSx3143_c2Qwfnnk?usp=share_link' target='_blank'>alternative Google Drive mirror.</a>");
-    //   // } else {
-    //   //   // $(".loginBtn").show();
-    //   //   // $(".logoutBtn").hide();
-    //   //   // $(".secret").hide();
-    //   //   // $(".user_name").text("");
-    //   //   // $(".login-message").text("You are currently not authenticated.");
-    //   // }
-    // }
+        if (isAuthenticated) {
+          await updateUI(auth0Client, query);
+        }
+      })
+    }
   });
 
 
@@ -293,7 +280,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       var queryParams = new URLSearchParams(query);
       var redirectUri = queryParams.get("return");
       if (redirectUri) {
-       //console.log("we were gonna redirect here to ", "/" + redirectUri)
+        //console.log("we were gonna redirect here to ", "/" + redirectUri)
         window.location.href = "/" + redirectUri; // important: this also strips out queryParams so we don't infinitely redirect :)
       }
 
@@ -303,7 +290,7 @@ document.addEventListener('DOMContentLoaded', async () => {
       $(".welcome-pill-message").show();
       $(".logout-button").show();
       $(".welcome-pill-message").prop("value", `Welcome, ${user.nickname}`)
-      
+
     } else {
       $(".login-button").show();
       $(".welcome-pill-message").hide();
@@ -315,8 +302,6 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
   };
 
-  $(".secret").hide();
-
   const auth0_domain = 'ieeevis.us.auth0.com'
   const auth0_client_id = 'G8onz2A6h59RmuYFUbSLpGmxsGHOyPOv'
   //console.log("origin is " + window.location.origin);
@@ -324,118 +309,69 @@ document.addEventListener('DOMContentLoaded', async () => {
     domain: auth0_domain,
     clientId: auth0_client_id,
     cacheLocation: "localstorage"
-    }).then(async (auth0Client) => {
-      // const loginButton = document.getElementById("loginButton");
-      // // auth0Client.getTokenSilently()
-  
-      // loginButton.addEventListener("click", (e) => {
-      //   e.preventDefault();
-      //   auth0Client.loginWithRedirect();
-      // });
-        
-      // if (location.search.includes("state=") && 
-      //     (location.search.includes("code=") || 
-      //     location.search.includes("error="))) {
-      //   await auth0Client.handleRedirectCallback();
-      //   window.history.replaceState({}, document.title, "/");
-      // }
-  
-      // Assumes a button with id "logout" in the DOM
-      // const logoutButton = document.getElementById("logout");
-  
-      // logoutButton.addEventListener("click", (e) => {
-      //   e.preventDefault();
-      //   auth0Client.logout();
-      // });
+  }).then(async (auth0Client) => {
+    const isAuthenticated = await auth0Client.isAuthenticated();
+    const query = window.location.search;
+    const userProfile = await auth0Client.getUser();
 
-      // await auth0Client.getTokenSilently()
-  
-      const isAuthenticated = await auth0Client.isAuthenticated();
-      const query = window.location.search;
-      const userProfile = await auth0Client.getUser();
-  
-      console.log(isAuthenticated, 'authenticated')
-      console.log(userProfile)
+    console.log(isAuthenticated, 'authenticated')
+    console.log(userProfile)
 
-      // const welcomePill = document.getElementById('welcomePill')
-      // const loginBtn = document.getElementById('loginButton')
+    if (isAuthenticated) {
+      await updateUI(auth0Client, query);
+    }
+    else if (query.includes("code=") && query.includes("state=")) {
+      // NEW - check for the code and state parameters
+      // Process the login state
+      //console.log("WE ARE IN THE REDIRECT CALLBACK!!!!!!")
+      auth0Client
+        .handleRedirectCallback()
+        .then((cb) => {
+          console.log(cb, "--- cb");
+          window.history.replaceState({}, document.title, "/");
+          updateUI(auth0Client, query);
+        })
+        .catch((e) => {
+          // eslint-disable-next-line no-console
+          console.log(e, "--- error");
+        });
 
-      if (isAuthenticated)
-      {
-        // document.body.style.display = null;
-        await updateUI(auth0Client, query);
-        //console.log("WE GOT IN HERE, IS AUTHENTICATED IS TRUE")
-        // welcomePill.classList.remove('hidden')
-        // welcomePill.innerText = `Welcome, ${userProfile.nickname}`
-
-        // loginBtn.classList.add('hidden')
-      }
-      else if (query.includes("code=") && query.includes("state=")) {
-        // NEW - check for the code and state parameters
-        // Process the login state
-        //console.log("WE ARE IN THE REDIRECT CALLBACK!!!!!!")
-        auth0Client
-          .handleRedirectCallback()
-          .then((cb) => {
-            console.log(cb, "--- cb");
-            window.history.replaceState({}, document.title, "/");
-            updateUI(auth0Client, query);
-          })
-          .catch((e) => {
-            // eslint-disable-next-line no-console
-            console.log(e, "--- error");
-          });
-
-        // Use replaceState to redirect the user away and remove the querystring parameters
-      } else if (
-        window.location.href.includes("redirect") &&
-        !window.location.hash.includes("redirect")
-      ) {
-        console.log("included redirect")
-        // we should only trigger login requests if we have a page to return to
-        if (query.includes("return=")) {
-          // await auth0Client.loginWithRedirect({
-          //   redirect_uri: window.location.href,
-          //   authorizationParams: {
-          //     redirect_uri: window.location.href,
-          //   }
-          // });
-          await auth0Client.loginWithPopup();
-
-          updateUI(auth0Client, query)
-        }
-      } else  if ( window.location.href.includes("room_") || window.location.href.includes("paper_") ){
-          // window.location.href = `/year/2024/program/redirect?return=${window.location.pathname.slice(1)}`;
-
-          window.location.href = "/year/2024/welcome?loginMsg=true";
-        }
-
-      // {
-        // welcomePill.classList.add('hidden')
-        // loginBtn.classList.remove('hidden')
-
-      // }
-      //console.log("there is a log in button, I swear!");
-      $(".login-button").click(async function () {
-        //console.log("HELLO IN LOGIN BUTTON");
+      // Use replaceState to redirect the user away and remove the querystring parameters
+    } else if (
+      window.location.href.includes("redirect") &&
+      !window.location.hash.includes("redirect")
+    ) {
+      console.log("included redirect")
+      // we should only trigger login requests if we have a page to return to
+      if (query.includes("return=")) {
         // await auth0Client.loginWithRedirect({
-        //   // redirect_uri: `${window.location.origin}/program/redirect.html?return=${window.location.href}`,
+        //   redirect_uri: window.location.href,
         //   authorizationParams: {
-        //     redirect_uri: `${window.location.origin}`,
-        //     // redirect_uri: `${window.location.origin}/program/redirect.html?return=${window.location.href}`,
+        //     redirect_uri: window.location.href,
         //   }
         // });
         await auth0Client.loginWithPopup();
+
         updateUI(auth0Client, query)
+      }
+    } else if (window.location.href.includes("room_") || window.location.href.includes("paper_")) {
+      // window.location.href = `/year/2024/program/redirect?return=${window.location.pathname.slice(1)}`;
+
+      window.location.href = "/year/2024/welcome?loginMsg=true";
+    }
+
+    $(".login-button").click(async function () {
+      await auth0Client.loginWithPopup();
+      updateUI(auth0Client, query)
+    });
+    $(".logoutBtn").click(async function () {
+      await auth0Client.logout({
+        redirect_uri: window.location.href,
       });
-      $(".logoutBtn").click(async function () {
-        await auth0Client.logout({
-          redirect_uri: window.location.href,
-        });
-      });
+    });
 
 
-    })
+  })
 
 
   // automatically highlight TOC sidebar entries
