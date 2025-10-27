f3cf5058d4fb9d5606c05a5a2dff83d5054dee87
harryli0088 <harryli0088@gmail.com>
2025-10-27 13:59:43 -0400

    images and thumbnails for papers, fixed auth0protect.js

--- static/2025/js/modules/auth0protect.js
+++ static/2025/js/modules/auth0protect.js
@@ -1,54 +1,28 @@
 window.onload = async () => {
   document.body.style.display = "none";
   $(".secret").hide();
-  const auth0 = await createAuth0Client({
+  const auth0Client = await auth0.createAuth0Client({
     domain: auth0_domain,
-    client_id: auth0_client_id,
-    cacheLocation: "localstorage",
-  });
+    clientId: auth0_client_id,
+    cacheLocation: "localstorage"
+  })
 
-  const isAuthenticated = await auth0.isAuthenticated();
+  const isAuthenticated = await auth0Client.isAuthenticated();
   const query = window.location.search;
 
-  const updateUI = async () => {
-    const is_auth = await auth0.isAuthenticated();
-    if (is_auth) {
-      document.body.style.display = null;
-
-      // if we have a redirect request, grab it and push the user over to that page they wanted to see
-      var queryParams = new URLSearchParams(query);
-      var redirectUri = queryParams.get("return");
-      if (redirectUri)
-        window.location.href = "/" + redirectUri; // important: this also strips out queryParams so we don't infinitely redirect :)
-
-      // unused atm, hook up later; this won't get executed since we change location above
-      const user = await auth0.getUser();
-      $(".loginBtn").hide();
-      $(".logoutBtn").show();
-      $(".secret").show();
-      $(".user_name").text(user.name);
-      $(".login-message").text("You are logged in as");
-    } else {
-      $(".loginBtn").show();
-      $(".logoutBtn").hide();
-      $(".secret").hide();
-      $(".user_name").text("");
-      $(".login-message").text("You are currently not authenticated.");
-    }
-  };
-
   if (isAuthenticated) {
-    document.body.style.display = null;
-    await updateUI();
-  } else if (query.includes("code=") && query.includes("state=")) {
+    await updateUI(auth0Client, query);
+  }
+  else if (query.includes("code=") && query.includes("state=")) {
     // NEW - check for the code and state parameters
     // Process the login state
-    auth0
+    //console.log("WE ARE IN THE REDIRECT CALLBACK!!!!!!")
+    auth0Client
       .handleRedirectCallback()
       .then((cb) => {
-        // console.log(cb, "--- cb");
+        console.log(cb, "--- cb");
         window.history.replaceState({}, document.title, "/");
-        updateUI();
+        updateUI(auth0Client, query);
       })
       .catch((e) => {
         // eslint-disable-next-line no-console
@@ -57,27 +31,127 @@ window.onload = async () => {
 
     // Use replaceState to redirect the user away and remove the querystring parameters
   } else if (
-    window.location.href.includes("redirect.html") &&
-    !window.location.hash.includes("redirect.html")
+    window.location.href.includes("redirect") &&
+    !window.location.hash.includes("redirect")
   ) {
+    console.log("included redirect")
     // we should only trigger login requests if we have a page to return to
     if (query.includes("return=")) {
-      await auth0.loginWithRedirect({
-        redirect_uri: window.location.href,
-      });
+      // await auth0Client.loginWithRedirect({
+      //   redirect_uri: window.location.href,
+      //   authorizationParams: {
+      //     redirect_uri: window.location.href,
+      //   }
+      // });
+      await auth0Client.loginWithPopup();
+
+      updateUI(auth0Client, query)
     }
-  } else {
-    window.location.href = `redirect.html?return=${window.location.pathname.slice(1)}`;
+  } else if (window.location.href.includes("room_") || window.location.href.includes("paper_") || window.location.href.includes("poster_") || window.location.href.includes("session_")) {
+    // window.location.href = `/year/2025/program/redirect?return=${window.location.pathname.slice(1)}`;
+
+    window.location.href = `/year/2025/welcome?loginMsg=true&return=${window.location.pathname.slice(1)}`;
   }
 
-  // $(".loginBtn").click(async function () {
-  //   await auth0.loginWithRedirect({
-  //     redirect_uri: `redirect.html?return=${window.location.href}`,
-  //   });
+
+  // $(".login-button").click(async function () {
+  //   await auth0Client.loginWithPopup();
+  //   updateUI(auth0Client, query)
   // });
-  // $(".logoutBtn").click(async function () {
-  //   await auth0.logout({
+
+  Array.from(document.getElementsByClassName('login-button')).forEach((item) => {
+    item.addEventListener('click', async () => {
+      await auth0Client.loginWithPopup();
+      updateUI(auth0Client, query)
+    })
+  })
+
+
+  Array.from(document.getElementsByClassName('logout-button')).forEach((item) => {
+    item.addEventListener('click', async () => {
+      await auth0Client.logout({
+        redirect_uri: window.location.href,
+      });
+    })
+  })
+
+  // document.getElementsByClassName('login-button').addEventListener('click', async () => {
+  //   await auth0Client.loginWithPopup();
+  //   updateUI(auth0Client, query);
+  // })
+
+  // $(".logout-button").click(async function () {
+  //   await auth0Client.logout({
   //     redirect_uri: window.location.href,
   //   });
   // });
+};
+
+
+const updateUI = async (auth0, query) => {
+  const is_auth = await auth0.isAuthenticated();
+  //console.log("are we auth?", is_auth)
+  if (is_auth) {
+    document.body.style.display = null;
+
+    let toast = document.getElementById('loginToastMessage');
+    if (toast) {
+      toast.style.display = "none";
+    }
+
+    // if we have a redirect request, grab it and push the user over to that page they wanted to see
+    var queryParams = new URLSearchParams(query);
+    var redirectUri = queryParams.get("return");
+    if (redirectUri) {
+      //console.log("we were gonna redirect here to ", "/" + redirectUri)
+      window.location.href = "/" + redirectUri; // important: this also strips out queryParams so we don't infinitely redirect :)
+    }
+
+    // unused atm, hook up later; this won't get executed since we change location above
+    const user = await auth0.getUser();
+    // $(".login-button").hide();
+    // $(".welcome-pill-message").show();
+    // $(".logout-button").show();
+    // $(".welcome-pill-message").prop("value", `Welcome, ${user.nickname}`)
+
+    Array.from(document.getElementsByClassName('login-button')).map((item) => {
+      item.classList.add('hide-auth-controls')
+    })
+
+    Array.from(document.getElementsByClassName('welcome-pill')).map((item) => {
+      item.classList.remove('hide-auth-controls')
+    })
+
+    Array.from(document.getElementsByClassName('discord-public-link')).map((item) => {
+      item.classList.remove('hide-auth-controls')
+    })
+    // Array.from(document.getElementsByClassName('logout-button')).map((item) => {
+    //   item.classList.remove('hide-auth-controls')
+    // })
+
+    // Array.from(document.getElementsByClassName('welcome-pill-message')).map((item) => {
+    //   item.value = `Welcome, ${user.nickname}`
+    // })
+
+  } else {
+    // $(".login-button").show();
+    Array.from(document.getElementsByClassName('login-button')).map((item) => {
+      item.classList.remove('hide-auth-controls')
+    })
+    // $(".welcome-pill-message").hide();
+    // $(".logout-button").hide();
+
+    // Array.from(document.getElementsByClassName('logout-button')).map((item) => {
+    //   item.classList.add('hide-auth-controls')
+    // })
+
+    // Array.from(document.getElementsByClassName('welcome-pill-message')).map((item) => {
+    //   item.attributes.value = ''
+    // })
+
+    Array.from(document.getElementsByClassName('welcome-pill')).map((item) => {
+      item.classList.add('hide-auth-controls')
+    })
+
+  }
 };
