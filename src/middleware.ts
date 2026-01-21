aefdd0a922cc853268bc9b2598982d78dcb8066f
harryli0088 <harryli0088@gmail.com>
2026-01-21 14:41:44 -0500

    updated CSP for dev environment

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,22 +4,43 @@ import type { MiddlewareHandler } from "astro";
 export const onRequest: MiddlewareHandler = async (context, next) => {
   const response = await next();
 
-  //CSP is currently in report only mode and will be enforced in the future
+  const isDev = import.meta.env.DEV;
+
+  //CONTENT SECURITY POLICY
   response.headers.set(
+    //CSP is currently in report only mode and will be enforced in the future
     "Content-Security-Policy-Report-Only",
     [
       "default-src 'self'",
       "base-uri 'self'",
       "object-src 'none'",
       "frame-ancestors 'none'",
-      "img-src 'self' data:",
-      "font-src 'self' https://fonts.gstatic.com/s/zillaslab/ https://fonts.gstatic.com/s/materialicons/ https://fonts.gstatic.com/s/firasans/",
-      "style-src 'self' https://fonts.googleapis.com",
-      "script-src 'self'",
-      "connect-src 'self'",
       "form-action 'self'",
-      "upgrade-insecure-requests",
-    ].join("; "),
+
+      // IMAGES
+      "img-src 'self' data:",
+
+      // FONTS
+      "font-src 'self' https://fonts.gstatic.com/s/zillaslab/ https://fonts.gstatic.com/s/materialicons/ https://fonts.gstatic.com/s/firasans/",
+
+      // STYLES
+      isDev
+        ? "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com"
+        : "style-src 'self' https://fonts.googleapis.com",
+
+      // SCRIPTS
+      isDev
+        ? "script-src 'self' 'unsafe-inline' 'unsafe-eval'"
+        : "script-src 'self'",
+
+      // NETWORK (HMR, APIs, etc.)
+      isDev ? "connect-src 'self' ws: http:" : "connect-src 'self'",
+
+      // Enforce HTTPS in prod only
+      !isDev && "upgrade-insecure-requests",
+    ]
+      .filter(Boolean)
+      .join("; "),
   );
 
   return response;
