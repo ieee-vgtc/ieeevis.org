436d9df813364a202b7f8c432ef878984fb07ff1
Shayan Monadjemi <83i@mac139026.ornl.gov>
2026-02-03 10:24:43 -0500

    fix broken prospectus link (still from 2025)

--- src/components/Supporters.tsx
+++ src/components/Supporters.tsx
@@ -13,7 +13,7 @@ export default function Supporters({
         <div className="w-full lg:w-1/3 mb-12 text-measure">
           <h2 className="mb-6 ">{title}</h2>
           <p className="mb-6 text-primary text-xl">{description}</p>
-          <a className="button button-blue" href={withBaseUrl(button_link)}>
+          <a className="button button-blue" href={withBaseURL(button_link)}>
             {button_text}
           </a>
         </div>
