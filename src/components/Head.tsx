8659a0babca618d2e567e6f2145a9bad176cb3eb
Andrew McNutt <mcnutt.andrew@gmail.com>
2026-05-11 12:53:35 -0600

    Astro fix (#2498)

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -17,7 +17,7 @@ export default function Head({
   }
 
   return (
-    <head>
+    <>
       <meta charSet="utf-8" />
 
       <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -54,22 +54,10 @@ export default function Head({
         href={`${import.meta.env.BASE_URL}/assets/vis2026_icon.svg`}
       />
 
-      {/* <!-- Include Material Icons --> */}
-      <link
-        href="https://fonts.googleapis.com/icon?family=Material+Icons"
-        rel="stylesheet"
-      />
-
-      {/* <!-- Include fonts --> */}
-      <link
-        href="https://fonts.googleapis.com/css?family=Fira+Sans:400,700|Zilla+Slab:400,500,700&display=swap"
-        rel="stylesheet"
-      />
-
       <link
         rel="sitemap"
         href={`${import.meta.env.BASE_URL}/sitemap-index.xml`}
       />
-    </head>
+    </>
   );
 }
