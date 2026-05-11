67589fa0a29f4c79c4275999fb8d229cc02eab18
Andrew McNutt <mcnutt.andrew@gmail.com>
2026-05-11 09:03:07 -0600

    add a link checker

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,21 @@ import tailwindcss from "@tailwindcss/vite";
 //https://docs.astro.build/en/guides/integrations-guide/sitemap/
 import sitemap from "@astrojs/sitemap";
 import rehypeExternalLinks from "rehype-external-links";
+// @ts-ignore
+import brokenLinksChecker from "astro-broken-links-checker";
 
 // https://astro.build/config
 export default defineConfig({
   base: process.env.BASE_PATH || "/year/2026", //this can be accessed in tsx and astro as import.meta.env.BASE_URL
-  integrations: [react(), sitemap()],
+  integrations: [
+    react(),
+    sitemap(),
+    brokenLinksChecker({
+      logFilePath: "broken-links.log",
+      checkExternalLinks: false,
+      throwError: true,
+    }),
+  ],
   site: process.env.SITE,
   markdown: {
     rehypePlugins: [
