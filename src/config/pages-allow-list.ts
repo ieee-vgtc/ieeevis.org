4a031eb0d0c92653759c8de300600c0cde6162c1
Andrew McNutt <mcnutt.andrew@gmail.com>
2026-07-10 16:08:31 -0600

    still block

--- src/config/pages-allow-list.ts
+++ src/config/pages-allow-list.ts
@@ -57,7 +57,7 @@ export const features = {
     // "/info/registration-and-travel",
     "/info/social-events",
     // for working on the program pages, we want to disable the program page until it's ready
-    // "/program",
+    "/program",
   ],
 } as const;
 
